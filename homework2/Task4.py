468ed39cacd5a4345d0f142e831618d5990a721e
Tsyuliou <tsyuliou@gmail.com>
2019-02-25 01:57:20 +0300

    8

--- homework2/Task4.py
+++ homework2/Task4.py
@@ -4,4 +4,4 @@ def print_formatted(number):
         for i in range(number):
             i += 1
             print('{0:{width}d} {0:{width}o} {0:{width}X} \
-            	{0:{width}b}'.format(i, width=width))
+                {0:{width}b}'.format(i, width=width))
