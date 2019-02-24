bcf0c0eb7b8196c88ed96189646161f46558b3b3
Tsyuliou <tsyuliou@gmail.com>
2019-02-25 01:52:46 +0300

    6

--- homework2/Task3.py
+++ homework2/Task3.py
@@ -9,17 +9,17 @@ while True:
         print("input n - correct")
         break
     else:
-        print("input n - incorrect")    
+        print("input n - incorrect")
 area = []
 for i in range(n):
-    while True:        
-        xy = list(map(int, input("Введите коорд {}-го: ".format(i+1)).split()))
+    while True:
+        xy = list(map(int, input("Введите коорд {}-го: ".format(i + 1)).split()))
         if 0 <= xy[0] <= xy[2] <= w and 0 <= xy[1] <= xy[3] <= h:
             break
         else:
-            print("something wrong")     
-    for x in range(xy[0], xy[2]+1):
-        for y in range(xy[1], xy[3]+1):
+            print("something wrong")
+    for x in range(xy[0], xy[2] + 1):
+        for y in range(xy[1], xy[3] + 1):
             area.append((x, y))
-S = w*h - len(set(area))
+S = w * h - len(set(area))
 print(S)
