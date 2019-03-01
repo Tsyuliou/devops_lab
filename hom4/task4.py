1e34d141eda7edb624d9114131456403d410f216
Tsyuliou <tsyuliou@gmail.com>
2019-03-01 15:15:28 +0300

    13

--- hom4/task4.py
+++ hom4/task4.py
@@ -1,30 +1,32 @@
 import requests
-import configparser
-
-config = configparser.ConfigParser()
-config.read("config.txt")
-username = config["private"]["username"]
-password = config["private"]["password"]
-link1 = config["data"]["link1"]
-link2 = config["data"]["link2"]
-git_r1 = requests.get(link1, auth=(username, password)).json()
-git_r2 = requests.get(link2, auth=(username, password)).json()
-print("Author of this work: {}".format(git_r1["login"]))
+import argparse
+import getpass
 
 
-class rep:
-    def rep_info(self, usr, passwd):
-        git_r2 = requests.get(link2, auth=(usr, passwd)).json()
-        '''with open('output.txt', 'a') as file:
-            file.write(str(git_r2))'''
-        print("ID: %s" % str(git_r2[0]["id"]))
-        print("Type: %s" % str(git_r2[0]["type"]))
-        print("Actor: %s" % str(git_r2[0]["actor"]))
-        print("Login: %s" % str(git_r2[0]["actor"]["login"]))
-        print("Display_login: %s" % str(git_r2[0]["actor"]["display_login"]))
-        print("Repo: %s" % str(git_r2[0]["repo"]))
+def rep_info(usr, passwd):
+    git_r1 = requests.get(link1, auth=(usr, passwd)).json()
+    git_r2 = requests.get(link2, auth=(usr, passwd)).json()
+    print(git_r2)
+    print("-----------------------------------------------")
+    print("Author of this work: {}".format(git_r1["login"]))
+    print("-----------------------------------------------")
+    print("Title: {}".format(git_r2["title"]))
+    print("Created_at: {}".format(git_r2["created_at"]))
+    print("Updated_at: {}".format(git_r2["updated_at"]))
+    print("Login: {}".format(git_r2["user"]["login"]))
+    print("Organizations_url: {}".format(git_r2["user"]["organizations_url"]))
+    print("Repos_url: {}".format(git_r2["user"]["followers_url"]))
 
 
-if __name__ == '__main__':
-    task4 = rep()
-    task4.rep_info(username, password)
+password = getpass.getpass()
+parser = argparse.ArgumentParser(prog='rep_info')
+parser.add_argument('username', help='Username to login on github')
+parser.add_argument('link1', help='Link to your repo')
+parser.add_argument('link2', help='Link to pull request')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+args = parser.parse_args()
+username = args.username
+link1 = args.link1
+link2 = args.link2
+
+rep_info(username, password)
